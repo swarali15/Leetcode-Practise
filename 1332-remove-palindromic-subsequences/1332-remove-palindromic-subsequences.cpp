11f11eebe188e9ac54ebb8017fd6f4f51fe25a64
Swarali Purandare <swarali.purandare18@vit.edu>
2022-06-08 16:49:19 +0530

    Time: 2 ms (43.62%), Space: 6.3 MB (26.79%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int removePalindromeSub(string s) 
     {
-        return 2 - (s== string(s.rbegin() , s.rend()))- s.empty();
+        //condition ? exp1 :exp2 ;
+        return s.empty() ? 0 : (s==string(s.rbegin(), s.rend())) ? 1:2;
         
     }
 };
